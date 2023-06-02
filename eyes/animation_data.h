934a2b852f79a2c3eae21227b14d9f0591b7315c
pheelwell <57875510+pheelwell@users.noreply.github.com>
2023-06-02 19:01:23 +0200

    initial commit

---
+++ eyes/animation_data.h
@@ -0,0 +1,8 @@
+#ifndef ANIMATION_DATA_H
+#define ANIMATION_DATA_H
+
+extern const unsigned char eyes_blink[4][1024];
+extern const unsigned char eyes_look_side[7][1024];
+extern const unsigned char eyes_look_side_down[10][1024];
+extern const unsigned char eyes_bigger[4][1024];
+extern const unsigned char eyes_bigger_side[5][1024];
---
+++ eyes/eyes.h
@@ -0,0 +1,2 @@
+const unsigned char eyes[7] [7936];
+const unsigned char eyes2[20] [7936];
